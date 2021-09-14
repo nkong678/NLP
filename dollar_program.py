826bab67914aa61f9ea950cdd8ded38574f25100
nkong678 <nkong678@gmail.com>
2021-09-14 16:21:09 -0400

    Update dollar_program.py

--- dollar_program.py
+++ dollar_program.py
@@ -6,14 +6,12 @@ Created on Tue Sep 14 15:06:26 2021
 """
 
 import nltk
-from nltk.corpus import gutenberg
-emma = gutenberg.words('austen-emma.txt')
-for fileid in gutenberg.fileids():
-    num_chars = len(gutenberg.raw(fileid))
-    num_words = len(gutenberg.words(fileid))
-    num_sents = len(gutenberg.sents(fileid))
-    num_vocab = len(set(w.lower() for w in gutenberg.words(fileid)))
-    print(round(num_chars/num_words), round(num_words/num_sents), round(num_words/num_vocab), fileid)
-macbeth_sentences = gutenberg.sents('shakespeare-macbeth.txt')
-print(macbeth_sentences)
-print(macbeth_sentences[1116])
+import sys
+print(len(sys.argv))
+if (len(sys.argv) == 1):
+    fname = input("Please enter the filename.\n")
+else:
+    fname = sys.argv[0]
+f = open(fname)
+raw = f.read()
+print(raw)
